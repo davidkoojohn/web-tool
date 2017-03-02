232ca47b16fee1c5e4204ae9d6d3d63a10b85de1
davidkoojohn <davidkoojohn@users.noreply.github.com>
2017-03-02 20:59:09 +0800

    Create rem.js

---
+++ javascript/rem.js
@@ -0,0 +1,22 @@
+/*
+ * @description : javaScript recalc_@_clientWidth
+ * 				: reset_@_font-size
+ * @author   : koojohn(郭钊)
+ * @date     : 2016-08-22
+ * 移动端rem的使用方法	PS:rem == 100px
+ */
+(function(){
+	resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+	recalc = function (){
+		var cWidth = document.documentElement.clientWidth || document.body.clientWidth;
+		if (!cWidth) return;
+		document.documentElement.style.fontSize = 200 * (cWidth / 640) + 'px';
+	};
+	if (!document.addEventListener) return;
+	window.addEventListener(resizeEvt, recalc, false);
+    document.addEventListener('DOMContentLoaded', recalc, false);
+})();
+
+
+
+
